87b7e4f8ff09074c50448381e64e9d60bfcc8bf7
Carlo Lucadei <clucadei@kseniasecurity.com>
2022-05-05 23:32:48 +0200

    Updated to a better solution for chapter-01

--- chapter-01/chapter-01.js
+++ chapter-01/chapter-01.js
@@ -1,10 +1,9 @@
 export const stringHasUniqueChars = (sentence) => {
     if (!sentence || sentence.length === 0)
         return false
-    let hasUniqueChars = true
-    for (let prevCharIndex = 0; prevCharIndex < sentence.length - 1 && hasUniqueChars; prevCharIndex++)
-        for (let nextCharIndex = prevCharIndex + 1; nextCharIndex < sentence.length && hasUniqueChars; nextCharIndex++)
-            if (sentence[prevCharIndex] === sentence[nextCharIndex])
-                hasUniqueChars = false
-    return hasUniqueChars
+    const chars = sentence.split('').sort()
+    for (let nextCharIndex = 1; nextCharIndex < chars.length; nextCharIndex++)
+        if (chars[nextCharIndex] === chars[nextCharIndex - 1])
+            return false
+    return true
 }
