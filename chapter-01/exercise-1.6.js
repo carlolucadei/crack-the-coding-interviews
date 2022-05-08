9b3a33524e7563d81bab54af3142c6f504b90942
Carlo Lucadei <clucadei@kseniasecurity.com>
2022-05-08 09:03:23 +0200

    Fixed exercise 1.6

--- chapter-01/exercise-1.6.js
+++ chapter-01/exercise-1.6.js
@@ -1,8 +1,16 @@
 export const rotateMatrixBy90DegreeClockWise = (matrix) => {
     const SIZE = matrix.length
-    let rotatedMatrix = new Array(SIZE).fill().map(() => new Array(SIZE).fill(''));
-    for (let i = 0; i < SIZE; i++)
-        for (let j = 0; j < SIZE; j++)
-            rotatedMatrix[SIZE - 1 - j][i] = matrix[i][j]
-    return rotatedMatrix
+    for (let step = 0; step < SIZE / 2; ++step) {
+        let first = step
+        let last = SIZE - 1 - step
+        for (let i = first; i < last; ++i) {
+            let offset = i - first
+            let top = matrix[first][i]
+            matrix[first][i] = matrix[last - offset][first]
+            matrix[last - offset][first] = matrix[last][last - offset]
+            matrix[last][last - offset] = matrix[i][last]
+            matrix[i][last] = top
+        }
+    }
+    return matrix
 }
